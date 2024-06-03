3bc21aec687a99ed704e5f643f431415cf6f3785
annatvali <anntvaliashvili@gmail.com>
2024-06-03 13:53:55 +0400

    feat: add create customer functionality

---
+++ lib/customer.ts
@@ -0,0 +1,55 @@
+import { client } from '@/lib/ClientBuilder';
+import { FormInputs } from '@/app/registration/constants';
+
+const generateId = () => {
+  return Math.random().toString(36).substring(2, 11);
+};
+
+export const createCustomer = async (data: FormInputs) => {
+  const shippingAddressIds: number[] = [];
+  const billingAddressIds: number[] = [];
+
+  if (data.sameBillingShipping) {
+    shippingAddressIds.push(0);
+    billingAddressIds.push(0);
+  } else if (data.differentShippingAddress) {
+    shippingAddressIds.push(0);
+  }
+
+  const customerDraft = {
+    email: data.email,
+    password: data.password,
+    firstName: data.firstName,
+    lastName: data.lastName,
+    dateOfBirth: data.dateOfBirth,
+    address: {
+      id: generateId(),
+      //TODO: write address data instead of empty strings
+      ...data.addresses[0],
+      firstName: data.firstName,
+      lastName: data.lastName,
+    },
+    isEmailVerified: true,
+    key: `${data.firstName.toLowerCase()}-${data.lastName.toLowerCase()}`,
+    shippingAddressIds,
+    billingAddressIds,
+    stores: [],
+  };
+
+  try {
+    const bodyRequest = JSON.stringify(customerDraft);
+    const projectKey = process.env.NEXT_PUBLIC_CTP_PROJECT_KEY;
+    const response = await client.execute({
+      uri: `/${projectKey}/customers`,
+      method: 'POST',
+      body: bodyRequest,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response.body;
+  } catch (error) {
+    throw error;
+  }
+};
