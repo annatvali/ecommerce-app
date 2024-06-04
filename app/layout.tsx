3171110315ab7f9a06d12ab7de168a5ee02b2035
annatvali <anntvaliashvili@gmail.com>
2024-06-04 21:51:36 +0400

    fix: export font poppins from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({
   weight: ['400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
 });
-const poppins = Poppins({
+export const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
 });
