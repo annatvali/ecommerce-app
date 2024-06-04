d08e5c3d76f43c19f059760938e605c58073955d
annatvali <anntvaliashvili@gmail.com>
2024-06-04 13:28:16 +0400

    chore: update jest setup & install required testing libraries fro the project

--- jest.setup.ts
+++
@@ -1,9 +0,0 @@
-// import '@testing-library/jest-dom/extend-expect';
-// const jestConfig = {
-//   // other configuration options...
-//   setupFilesAfterEnv: ['./jest.setup.ts'],
-// };
-
-// export default jestConfig;
-
-import '@testing-library/jest-dom/extend-expect';
