db950252b754f563f676154a6842b44860c91588
annatvali <anntvaliashvili@gmail.com>
2024-06-07 21:39:29 +0400

    chore: remove unnecessary margin from Button component

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -22,7 +22,7 @@ const Button: FC<ButtonProps> = ({
       href={{ href }}
       type="button"
       onClick={onClick}
-      className={`${textColor} bg-gradient-to-r ${colorClass} hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2`}
+      className={`${textColor} bg-gradient-to-r ${colorClass} hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center`}
     >
       {text}
     </Link>
