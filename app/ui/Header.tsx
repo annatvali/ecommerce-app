11c07a71924b044222068dddfaf4e88d45a4e30e
annatvali <anntvaliashvili@gmail.com>
2024-06-07 20:07:47 +0400

    feat: integrate BurgerMenu in Header and implement toggle functionality

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,14 +1,23 @@
+'use client';
+
+import { useState } from 'react';
 import Navigation from './Navigation';
+import BurgerMenu from './BurgerMenu';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <header className="w-full flex justify-between items-center gap-8 bg-gray-800 text-white p-4">
+    <header className="w-full flex justify-between items-center bg-gray-800 text-white p-4 relative z-20">
       <div>
-        <a className="text-xl font-bold" href="/">
+        <a className="text-xl font-bold hover:text-blue-300" href="/">
           CoMM
         </a>
       </div>
-      <Navigation />
+      <BurgerMenu isOpen={isOpen} toggleMenu={toggleMenu} />{' '}
+      <Navigation isOpen={isOpen} onLinkClick={toggleMenu} />
     </header>
   );
 };
